1b6f4caac6ac0d84ca2bc3496f0a0ac98607dad2
GhostProducerEDM <Yourghostproduceredm@gmail.com>
2025-05-17 20:30:08 +0500

    Update producttracksoriginal.js

--- producttracksoriginal.js
+++ producttracksoriginal.js
@@ -3,31 +3,35 @@ $(document).ready(function() {
         'VCL00001': 'https://drive.google.com/uc?export=download&id=10lfHKyJGcdRqGeO5AQoOci8UGOYL8xvC',
         'VCL00002': 'https://drive.google.com/uc?export=download&id=1125HgU5kyqUT7xcITADhZ3FDXfYMt0pa'
     };
-                  
-    function findProduct() {
-        var cProduct = $('.t706__product').length;
-        var newLinks = ''; // Создаем новую переменную для хранения ссылок
-        
-        for (let i = 0; i < cProduct; i++) { 
-            var artPrd = $('.t706__product-title:eq('+i+') div:last').html();
-            if (linkMass[artPrd]) { // Проверяем, существует ли такой артикул
-                newLinks += (newLinks ? '\n' : '') + (i+1) + ') ' + linkMass[artPrd];
-            }
-        };
-        
-        $('input[name="location"]').val(newLinks);
-    };   
     
-    // При клике на кнопку отправки
-    $('.t706 .t-submit').click(function(e) {
+    // Удаляем старые обработчики (если были)
+    $('.t706 .t-submit').off('click');
+    
+    // Вешаем новый обработчик
+    $('.t706 .t-submit').on('click', function(e) {
         e.preventDefault();
-        findProduct();
+        
+        var links = []; // Массив для хранения ссылок (убьёт дубли)
+        
+        $('.t706__product').each(function() {
+            var artPrd = $(this).find('.t706__product-title div:last').text().trim();
+            
+            if (linkMass[artPrd] && !links.includes(linkMass[artPrd])) {
+                links.push((links.length + 1) + ') ' + linkMass[artPrd]);
+            }
+        });
+        
+        // Вставляем в поле
+        $('input[name="location"]').val(links.join('\n'));
+        
+        // Очищаем через 6 сек
         setTimeout(function() {
             $('input[name="location"]').val('');
         }, 6000);
     });
     
-    $(document).on("keydown", ".t706 form", function(event) {
+    // Блокируем Enter
+    $(document).on("keydown", ".t706 form", function(event) { 
         return event.key != "Enter";
     });
 });
