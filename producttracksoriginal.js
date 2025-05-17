b59ea8fc858056446991686b538416cb09af2875
GhostProducerEDM <Yourghostproduceredm@gmail.com>
2025-05-17 20:34:57 +0500

    Update producttracksoriginal.js

--- producttracksoriginal.js
+++ producttracksoriginal.js
@@ -3,35 +3,44 @@ $(document).ready(function() {
         'VCL00001': 'https://drive.google.com/uc?export=download&id=10lfHKyJGcdRqGeO5AQoOci8UGOYL8xvC',
         'VCL00002': 'https://drive.google.com/uc?export=download&id=1125HgU5kyqUT7xcITADhZ3FDXfYMt0pa'
     };
+
+    // 1. Блокируем повторные выполнения
+    var isProcessing = false;
     
-    // Удаляем старые обработчики (если были)
-    $('.t706 .t-submit').off('click');
-    
-    // Вешаем новый обработчик
-    $('.t706 .t-submit').on('click', function(e) {
-        e.preventDefault();
+    // 2. Полностью переработанная функция
+    function generateLinks() {
+        if (isProcessing) return;
+        isProcessing = true;
         
-        var links = []; // Массив для хранения ссылок (убьёт дубли)
-        
+        var uniqueLinks = {};
         $('.t706__product').each(function() {
             var artPrd = $(this).find('.t706__product-title div:last').text().trim();
-            
-            if (linkMass[artPrd] && !links.includes(linkMass[artPrd])) {
-                links.push((links.length + 1) + ') ' + linkMass[artPrd]);
+            if (linkMass[artPrd]) {
+                uniqueLinks[linkMass[artPrd]] = true; // Хэш для уникальности
             }
         });
         
-        // Вставляем в поле
-        $('input[name="location"]').val(links.join('\n'));
+        var result = Object.keys(uniqueLinks).map(function(link, index) {
+            return (index + 1) + ') ' + link;
+        }).join('\n');
         
-        // Очищаем через 6 сек
+        $('input[name="location"]').val(result);
+        
         setTimeout(function() {
             $('input[name="location"]').val('');
+            isProcessing = false;
         }, 6000);
+    }
+    
+    // 3. Вешаем обработчик с защитой
+    $(document).off('click', '.t706 .t-submit').on('click', '.t706 .t-submit', function(e) {
+        e.preventDefault();
+        generateLinks();
     });
     
-    // Блокируем Enter
-    $(document).on("keydown", ".t706 form", function(event) { 
-        return event.key != "Enter";
+    // 4. Дополнительная защита для формы
+    $('.t706 form').off('submit').on('submit', function() {
+        generateLinks();
+        return true;
     });
 });
